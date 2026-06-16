c7363f829a42ee4da3f596d40f2d94577416fd2a
mohammed halhal <halhalmohammed2005@gmail.com>
2026-06-16 22:04:05 +0100

    Fix Vite base path location

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Do_you/', // 👈 It must be right here, outside of plugins
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-      },base: '/Do_you/',
+      },
     }),
   ],
 })
